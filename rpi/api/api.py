acb7a7054b7226afe10b539e9fcfee350f378818
Timotej979 <timotej.pet@gmail.com>
2023-10-26 16:35:12 +0200

    MongoDB service config, api in work

---
+++ rpi/api/api.py
@@ -0,0 +1,31 @@
+import logging, os, sys
+
+import asyncio
+import aiohttp_sqlalchemy as ahsa
+
+from aiohttp import web
+
+
+###############################################################
+# Get environment variables
+
+
+
+
+
+
+
+
+
+###############################################################
+# API class
+class APIManager() {
+
+
+
+
+
+
+
+
+}

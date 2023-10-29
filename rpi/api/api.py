01b408bbfe28ffbe51bd225e3b0ccd1413f4e217
Timotej979 <timotej.pet@gmail.com>
2023-10-29 16:28:33 +0100

    Add mongo motor driver config, /healthz-db working

--- rpi/api/api.py
+++ rpi/api/api.py
@@ -10,9 +10,16 @@ from aiohttp import web
 ####################################################### ENV VARIABLES #######################################################
 #############################################################################################################################
 
+# API configuration
 API_CONFIG = os.getenv("API_CONFIG")
 URL_PREFIX = str(os.getenv("API_PREFIX"))
+
+# DB configuration
 DB_CONNECTION_STRING = str(os.getenv("API_DB_CONNECTION_STRING"))
+DB_MIN_POOL_SIZE = int(os.getenv("API_DB_MIN_POOL_SIZE"))
+DB_MAX_POOL_SIZE = int(os.getenv("API_DB_MAX_POOL_SIZE"))
+DB_SERVER_SELECTION_TIMEOUT_MS = int(os.getenv("API_DB_SERVER_SELECTION_TIMEOUT_MS"))
+DB_MAX_IDLE_TIME_MS = int(os.getenv("API_DB_MAX_IDLE_TIME_MS"))
 
 ###############################################################################################################################
 ######################################################### API MANAGER #########################################################
@@ -25,31 +32,38 @@ class API_Manager():
 
     # Initialize API
     async def initialize_api(self):
-        log.info("Initializing API server")
+        log.info("## Initializing API server ##")
 
         self.subapp = web.Application()
 
-        log.info("Configuring Motor driver for MongoDB...")
+        log.info("## Configuring Motor driver for MongoDB... ##")
         self.subapp['db_client'] = await self.setup_db()
 
-        log.info("Adding routes to application object")
+        log.info("## Adding routes to application object... ##")
         self.subapp.router.add_routes(self.routes)
 
         # Add sub-app to set the IP/recognition-api request
         self.app = web.Application()
         self.app.add_subapp(URL_PREFIX, self.subapp)
 
-        log.info("API initialization complete")
+        log.info("## API initialization complete ##")
 
     # Setup DB connection
     async def setup_db(self):
-        # Example connection string: mongodb://localhost:27017/database_name
-        client = AsyncIOMotorClient(DB_CONNECTION_STRING)
+        # Configure the Motor driver for MongoDB
+        client = AsyncIOMotorClient(host = DB_CONNECTION_STRING,
+                                    minPoolSize=DB_MIN_POOL_SIZE,
+                                    maxPoolSize=DB_MAX_POOL_SIZE,
+                                    serverSelectionTimeoutMS=DB_SERVER_SELECTION_TIMEOUT_MS,
+                                    maxIdleTimeMS=DB_MAX_IDLE_TIME_MS,
+                                    appname="Rainsim-API",
+                                    retryWrites=True,
+                                    retryReads=True)
         return client
 
     # Run API
     def run_api(self, host, port, loop):
-        log.info("Server starting on address: http://{}:{}".format(host, port))
+        log.info("## Server starting on address: http://{}:{} ##".format(host, port))
         web.run_app(self.app, host=host, port=port, loop=loop)
 
 
@@ -63,22 +77,35 @@ class API_Manager():
     # API healthcheck
     @routes.get('/healthz-api')
     async def api_health_check(request):
-        log.info("API health-check running\n")
+        log.info("## API health-check running ##\n")
         return web.Response(text="## API health-check successfull ##\n")
 
     # DB healthcheck
     @routes.get('/healthz-db')
     async def db_health_check(request):
-        log.info("DB health-check running")
+        log.info("## DB health-check running ##")
 
         try:
+            # Get DB client
             db = request.app['db_client']
-
-            # Check if DB is alive
-            info = await db.server_info()
-
-        return web.Response(text="## DB health-check successfull ##\n")
-
+        except Exception as e:
+            log.error("!! DB health-check failed with error: {}".format(e))
+            return web.Response(text="!! DB health-check failed !!\n")
+        else:
+            try:
+                # Check if DB is alive
+                info = await db.server_info()
+            except Exception as e:
+                log.error("!! DB health-check failed with error: {}".format(e))
+                return web.Response(text="!! DB health-check failed !!\n")
+            else:
+                # If DB is alive return success
+                log.info("## DB health-check successfull ##\n")
+                log.info("## DB info: {} ##\n".format(info))
+            finally:
+                return web.Response(text="## DB health-check successfull ##\n")
+            
+    
 
 
 
@@ -91,11 +118,22 @@ if __name__ == '__main__':
     if API_CONFIG == "dev":
         # Development build
         logging.basicConfig(level=logging.DEBUG)
-        # Limit DB connection pooling if necessary
+        
+        # Set DB connection parameters
+        DB_MIN_POOL_SIZE = 1
+        DB_MAX_POOL_SIZE = 1
+        DB_SERVER_SELECTION_TIMEOUT_MS = 5000
+        DB_MAX_IDLE_TIME_MS = 5000
+        
+        # Set logging and print configuration
         log = logging.getLogger()
-        log.info("Starting API server in development mode")
-        log.info("URL_PREFIX: {}".format(URL_PREFIX))
-        log.info("DB_CONNECTION_STRING: {}".format(DB_CONNECTION_STRING))
+        log.info("## Starting API server in development mode ##")
+        log.info("## URL_PREFIX: {} ##".format(URL_PREFIX))
+        log.info("## DB_CONNECTION_STRING: {} ##".format(DB_CONNECTION_STRING))
+        log.info("## DB_MIN_POOL_SIZE: {} ##".format(DB_MIN_POOL_SIZE))
+        log.info("## DB_MAX_POOL_SIZE: {} ##".format(DB_MAX_POOL_SIZE))
+        log.info("## DB_SERVER_SELECTION_TIMEOUT_MS: {} ##".format(DB_SERVER_SELECTION_TIMEOUT_MS))
+        log.info("## DB_MAX_IDLE_TIME_MS: {} ##".format(DB_MAX_IDLE_TIME_MS))
 
     elif API_CONFIG == "prod":
         # Production build
@@ -104,7 +142,10 @@ if __name__ == '__main__':
         log.info("Starting API server in production mode")
         log.info("URL_PREFIX: {}".format(URL_PREFIX))
         log.info("DB_CONNECTION_STRING: {}".format(DB_CONNECTION_STRING))
-
+        log.info("DB_MIN_POOL_SIZE: {}".format(DB_MIN_POOL_SIZE))
+        log.info("DB_MAX_POOL_SIZE: {}".format(DB_MAX_POOL_SIZE))
+        log.info("DB_SERVER_SELECTION_TIMEOUT_MS: {}".format(DB_SERVER_SELECTION_TIMEOUT_MS))
+        log.info("DB_MAX_IDLE_TIME_MS: {}".format(DB_MAX_IDLE_TIME_MS))
 
     else:
         # If APP_CONFIG env variable is not set abort start

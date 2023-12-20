4d895269ed303454c6adb811d1496ddca5113766
Timotej979 <timotej.pet@gmail.com>
2023-12-20 11:29:09 +0100

    Add working gray-image depth capture

--- testing/camera-test1.py
+++ testing/camera-test1.py
@@ -32,7 +32,7 @@ class RealSense():
         cv2.destroyAllWindows()
         self.pipeline.stop()
 
-    # Get frames from the camera RGB and depth
+    # Get frames from the camera: RGB and depth
     def get_rgb_frame(self):
         color_frame = self.frames.get_color_frame()
         if not color_frame:
@@ -45,22 +45,31 @@ class RealSense():
         depth_frame = self.frames.get_depth_frame()
         if not depth_frame:
             return None
-        # Convert the frame to a numpy array
-        depth_frame = np.asanyarray(depth_frame.get_data())
-        return depth_frame
+        # Get height and width of the depth frame
+        height = depth_frame.get_height()
+        width = depth_frame.get_width()
+        # Create a NumPy array to store depth values
+        depth_list = np.zeros((height, width), dtype=np.float32)
+        # Populate the depth values in the array
+        for x in range(height):
+            for y in range(width):
+                if x < depth_frame.get_height() and y < depth_frame.get_width():
+                    depth_list[x][y] = depth_frame.get_distance(y, x)
+        # Normalize the depth values to the range 0-255
+        print(depth_list)
+        return depth_list
 
-    def average_depth_frames(self, num_frames):
-        # Loop over the number of frames
-        for i in range(num_frames):
-            # Loop to always get a new frame
-            depth_frame = None
-            while(depth_frame == None):
-                depth_frame = self.get_depth_frame()
+
+    def get_average_depth_frame(self, num_frames):
+        # Set the avg to first frame
+        depth_frame_avg = self.get_depth_frame()
+        # Loop over the rest for average and divide by frame number
+        for i in range(num_frames - 1):
             # Add the frame to the average
-            depth_frame_avg = depth_frame_avg + depth_frame
+            depth_frame_avg = depth_frame_avg + self.get_depth_frame()
 
         # Average the frames
-        depth_frame_avg = num_frames
+        depth_frame_avg = depth_frame_avg / num_frames
         return depth_frame_avg
 
 
@@ -90,23 +99,32 @@ class RealSense():
         self.calibration_points = np.array([roi])
 
 
-    def difference_avg_frames(self, image, num_frames):
+    def calculate_diff_avg_frames(self, image, num_frames):
         # Get average depth frame
-        depth_frame1 = self.average_depth_frames(num_frames)
-        depth_frame2 = self.average_depth_frames(num_frames)
+        depth_frame1 = self.get_average_depth_frame(num_frames)
+        depth_frame2 = self.get_average_depth_frame(num_frames)
 
         # Compute the difference
         depth_frame_diff = depth_frame1 - depth_frame2
 
         return depth_frame_diff
 
-    def homographic_transform(self):
+    def homographic_transform(self, frame):
+        # Set the homography points
         points_I = self.calibration_points
-        points_J = [0, 300, 0, 300]
+        points_J = input("Enter the ratio of the rectangle sides (Example input: 1.1:2.3 ): ")
+        points_J = points_J.split(':')
+        points_J = np.array([0, int(points_J[0] * 100), 0, int(points_J[1] * 100)])
+        
         # Compute the homography
         H = cv2.getPerspectiveTransform(points_I, points_J)
+
+        # Get ROI for our case
+        roi = self.calibration_points
+
         # Apply the homography
-        dst = cv2.warpPerspective(image, H, (300, 300))
+        dst = cv2.warpPerspective(frame[roi[1]:roi[1]+roi[3], roi[0]:roi[0]+roi[2]], H, (500, 500))
+        
         return dst
 
 
@@ -125,8 +143,34 @@ if __name__ == '__main__':
         # Select calibration points
         cameraInstance.select_calibration_points()
 
-        
+        # Get RGB and depth frame
+        rgb_image = cameraInstance.get_rgb_frame()
+        depth_frame = cameraInstance.get_depth_frame()
 
+        # Display depth frame
+        cv2.imshow("Depth Frame", depth_frame)
+        key = cv2.waitKey(0)
+        if key == ord(' '):
+            cv2.destroyAllWindows()
+
+
+        # Calculate diff frame
+        #diff_frame = cameraInstance.calculate_diff_avg_frames(depth_frame, 5)
+
+        # Display the diff frame
+        #cv2.imshow("Diff Frame", diff_frame)
+        #key = cv2.waitKey(0)
+        #if key == ord(' '):
+        #    cv2.destroyAllWindows()
+
+        # Apply homographic transform
+        #dst = cameraInstance.homographic_transform(diff_frame)
+
+        # Display the diff frame
+        #cv2.imshow("Homographic Transform", dst)
+        #key = cv2.waitKey(0)
+        #if key == ord(' '):
+        #    cv2.destroyAllWindows()
 
 
     finally:

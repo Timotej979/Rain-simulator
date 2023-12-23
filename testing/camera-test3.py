11e22a680fa2ae49cb4de95e234577fbc51a1a32
Timotej979 <timotej.pet@gmail.com>
2023-12-23 21:55:41 +0100

    Add working ROI picker

--- testing/camera-test3.py
+++ testing/camera-test3.py
@@ -5,11 +5,25 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 class RealSenseCamera:
-    def __init__(self, num_frames=10):
+    """
+    Class for RealSense camera
+    Methods:
+        - get_rgb_frame(): Get RGB frame
+        - get_depth_frame(): Get depth frame
+        - normalize_depth_frame(depth_frame): Normalize depth frame
+        - calculate_average_depth_frame(): Calculate average depth frame
+        - calculate_difference_frames(frame1, frame2): Calculate difference frames
+        - run(): Run the camera
+
+    """
+
+    # Initialize the camera object
+    def __init__(self, num_frames=10, image_size_x=640, image_size_y=480):
+        # Create a pipeline
         self.pipeline = rs.pipeline()
         config = rs.config()
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, image_size_x, image_size_y, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.depth, image_size_x, image_size_y, rs.format.z16, 30)
 
         # Additional settings to optimize depth image quality
         profile = self.pipeline.start(config)
@@ -20,20 +34,46 @@ class RealSenseCamera:
         depth_sensor.set_option(rs.option.gain, 16)  # Adjust gain
         depth_sensor.set_option(rs.option.laser_power, 250)  # Adjust laser power
 
+        # General attributes
+        self.image_size_x = image_size_x
+        self.image_size_y = image_size_y
         self.num_frames = num_frames
         self.rgb_frames = []
         self.canvas = None
         self.is_running = False
-        self.start_roi_point = None
-        self.current_roi_rect_id = None
-        self.latest_roi_points = None  # Quadruple to store the latest ROI points
 
+        # Attributes for ROI selection
+        self.start_roi_point = None
+        self.roi_points = None 
+        self.roi_flag = False 
+
+
+    ##########################################################################################################################
+    # Stream management functions, use this to start and stop the camera streams
+    def start_streams(self):
+        self.is_running = True
+        self.update()
+
+    def stop_streams(self):
+        self.is_running = False
+
+
+    ##########################################################################################################################
+    # Get frames from the camera: RGB and depth
     def get_rgb_frame(self):
         frames = self.pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
         rgb_frame = np.asanyarray(color_frame.get_data())
         return rgb_frame
 
+    def get_depth_frame(self):
+        frames = self.pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        return depth_frame
+
+
+    ##########################################################################################################################
+    # Frame processing functions
     def normalize_depth_frame(self, depth_frame):
         depth_image = np.asanyarray(depth_frame.get_data())
         q1 = np.percentile(depth_image, 25)
@@ -41,46 +81,39 @@ class RealSenseCamera:
         normalized_depth = np.clip((depth_image - q1) / (q3 - q1), 0, 1) * 255
         return normalized_depth.astype(np.uint8)
 
+
+    ##########################################################################################################################
+    # ROI selection functions
     def set_roi(self, event):
         if self.is_running:
+            self.roi_flag = False
             x, y = event.x, event.y
             self.start_roi_point = (x, y)
-            self.current_roi_rect_id = None  # Reset the rectangle ID when starting a new selection
 
     def update_roi(self, event):
         if self.is_running and self.start_roi_point:
             x, y = event.x, event.y
-            self.draw_selection_rectangle(self.start_roi_point, (x, y))
+            self.roi_points = (self.start_roi_point[0], self.start_roi_point[1], x, y)
 
     def release_roi(self, event):
         if self.is_running and self.start_roi_point:
             x1, y1 = self.start_roi_point
             x2, y2 = event.x, event.y
 
-            # Draw the selected rectangle on the RGB frames
-            cv2.rectangle(self.rgb_frames, (x1, y1), (x2, y2), (255, 0, 0), 4)
-
-            # Update the Tkinter canvas with the modified frames
-            self.display_frames_tkinter(self.rgb_frames, self.normalize_depth_frame(self.get_depth_frame()))
-
-            # Keep the rectangle ID for future updates
-            self.current_roi_rect_id = self.draw_selection_rectangle((x1, y1), (x2, y2))
-
             # Store the latest ROI points
-            self.latest_roi_points = (x1, y1, x2, y2)
-            print("Latest ROI Points:", self.latest_roi_points)
+            self.roi_points = (x1, y1, x2, y2)
+            print("Latest ROI Points:", self.roi_points)
 
             # Reset the start_roi_point attribute
             self.start_roi_point = None
+            self.roi_flag = True
 
-    def draw_selection_rectangle(self, start_point, end_point):
-        # Draw the selection rectangle on the Tkinter canvas
-        if self.current_roi_rect_id:
-            self.canvas.delete(self.current_roi_rect_id)
-        x1, y1 = start_point
-        x2, y2 = end_point
-        return self.canvas.create_rectangle(x1, y1, x2, y2, outline="red", width=4)
+    def reset_roi(self):
+        self.roi_points = None
+        self.roi_flag = False
 
+    ##########################################################################################################################
+    # Window update function
     def update(self):
         if self.is_running:
             # Get current depth and RGB frames
@@ -99,15 +132,16 @@ class RealSenseCamera:
             # Schedule the update method to be called after a delay
             self.canvas.after(10, self.update)
 
+    ##########################################################################################################################
+    # Display function
     def display_frames_tkinter(self, rgb_frame, normalized_depth_frame):
+
         # Convert frames to RGB format for PIL
         rgb_image = Image.fromarray(cv2.cvtColor(rgb_frame, cv2.COLOR_BGR2RGB))
         depth_image = Image.fromarray(normalized_depth_frame)
 
-        # Resize images to fit on the screen with equal scaling
+        # Get size of the Tkinter canvas
         width, height = self.canvas.winfo_width() // 2, self.canvas.winfo_height() // 2
-        rgb_image = rgb_image.resize((width, height))
-        depth_image = depth_image.resize((width, height))
 
         # Convert images to PhotoImage
         rgb_photo = ImageTk.PhotoImage(rgb_image)
@@ -117,17 +151,37 @@ class RealSenseCamera:
         self.canvas.create_image(width, 0, anchor="nw", image=rgb_photo, tags="rgb")
         self.canvas.create_image(0, height, anchor="nw", image=depth_photo, tags="depth")
 
+        # Check if roi flag is true
+        if self.roi_flag:
+            if self.roi_points is not None:
+                # Get roi points
+                x, y, w, h = self.roi_points
+                # Crop the depth frame
+                normalized_depth_frame = normalized_depth_frame[y:h, x:w]
+                # Draw new rectangle with adjusted coordinates
+                self.canvas.create_rectangle(x, y, w, h, outline="red", tags="roi")
+        else:
+            if self.roi_points is not None:
+                # Get roi points
+                x, y, w, h = self.roi_points
+                # Delete previous rectangle
+                self.canvas.delete("roi")
+                # Draw new rectangle with adjusted coordinates
+                self.canvas.create_rectangle(x, y, w, h, outline="red", tags="roi")
+
         # Keep a reference to the images to prevent garbage collection
         self.canvas.rgb_photo = rgb_photo
         self.canvas.depth_photo = depth_photo
 
+    ##########################################################################################################################
+    # Main function
     def run(self):
         # Create a new Tkinter window
         root = tk.Tk()
         root.title("RealSense Camera GUI")
 
         # Create the main canvas
-        self.canvas = tk.Canvas(root, width=1920, height=1080)
+        self.canvas = tk.Canvas(root, width=1300, height=990)
         self.canvas.grid(row=0, column=0, columnspan=2, rowspan=2)
 
         # Create buttons/control section
@@ -146,6 +200,10 @@ class RealSenseCamera:
         stop_button = tk.Button(buttons_frame, text="Stop", command=self.stop_streams)
         stop_button.pack()
 
+        # Reset ROI button
+        reset_roi_button = tk.Button(buttons_frame, text="Reset ROI", command=self.reset_roi)
+        reset_roi_button.pack()
+
         # Bind mouse events for region of interest (ROI) selection
         self.canvas.bind("<ButtonPress-1>", self.set_roi)
         self.canvas.bind("<B1-Motion>", self.update_roi)
@@ -157,18 +215,7 @@ class RealSenseCamera:
         # Stop the RealSense pipeline when the Tkinter window is closed
         self.pipeline.stop()
 
-    def start_streams(self):
-        self.is_running = True
-        self.update()
-
-    def stop_streams(self):
-        self.is_running = False
-
-    def get_depth_frame(self):
-        frames = self.pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        return depth_frame
 
 if __name__ == "__main__":
-    camera = RealSenseCamera(num_frames=10)
+    camera = RealSenseCamera(num_frames=10, image_size_x=640, image_size_y=480)
     camera.run()
